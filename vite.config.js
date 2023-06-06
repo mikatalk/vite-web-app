fdebdb422e150a43c6865284ae8e4a4ae0cd7c2e
Mikatalk <mikatalk@users.noreply.github.com>
2023-06-05 20:10:00 -0700

    Coding

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,5 @@
 import { defineConfig } from 'vite';
-import { rollup } from 'rollup'
 import { VitePWA } from "vite-plugin-pwa"
-import { nodeResolve } from '@rollup/plugin-node-resolve'
-
-
-// const CompileServiceWorker = () => ({
-//   name: 'compile-typescript-service-worker',
-//   async writeBundle(_options, _outputBundle) {
-//     const inputOptions = {
-//       input: 'src/js/sw.js',
-//       plugins: [nodeResolve()],
-//     }
-//     const outputOptions = {
-//       file: 'dist/sw.js',
-//       format: 'es',
-//     }
-//     const bundle = await rollup(inputOptions)
-//     await bundle.write(outputOptions)
-//     await bundle.close()
-//   }
-// })
-
-
 
 export default ({ mode }) => {
     return defineConfig({
@@ -30,10 +8,6 @@ export default ({ mode }) => {
         VitePWA({
           registerType: 'autoUpdate',
           // includeAssets: ['vite.svg'],
-          // workbox: {
-          //   importScripts: ['src/js/sw-functional.js'],
-          //   globIgnores: ['**/node_modules/**/*', '**/sw.js'],
-          // },
           manifest: {
 
             "scope": "/vite-web-app/",
@@ -55,7 +29,6 @@ export default ({ mode }) => {
             "short_name": "Stack&Fit",
           }
         }),
-        // CompileServiceWorker()
       ]
     });
 }
