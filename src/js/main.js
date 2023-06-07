a6c9030ffc0742d7c4e2488f2bf21bc1bf37a77d
Mikatalk <mikatalk@users.noreply.github.com>
2023-06-06 18:44:03 -0700

    Coding again

--- src/js/main.js
+++ src/js/main.js
@@ -9,18 +9,50 @@ console.log('store:', store)
 console.log('cells:', store.state.grid.cells)
 
 const gridElement = document.querySelector('#app .grid');
+const cellElements = [];
+store.state.grid.cells.forEach(({value}) => {
+  const el = document.createElement('div');
+  el.classList.add('box');
+  value === 'set' && el.classList.add('active');
+  cellElements.push(el);
+  gridElement.appendChild(el);
+})
+
+let lastChangeId = 0, changeId = 1;
 update()
+store.doSomething();
+changeId += 1;
 
 function update () {
-  store.doSomething();
-  redraw();
   window.requestAnimationFrame(update);
+  
+  // store.doSomething();
+
+  store.doSomething();
+  changeId += 1;
+  
+  if (lastChangeId !== changeId) {
+    lastChangeId = changeId;
+    redraw();
+  }
+  
   // console.log('hi')
 }
 
 function redraw() {
-  gridElement.innerHTML = '<div class="grid-bg"></div>'
-  store.state.grid.cells.forEach(({value}) => {
-    gridElement.innerHTML += `<div class="box ${value === 'set' ? 'active' : ''}"></div>`
+  console.log('redraw')
+
+  store.state.grid.cells.forEach(({value}, index) => {
+    const el = cellElements[index];
+    if (value === 'set') {
+      el.classList.add('active');
+    } else {
+      el.classList.remove('active');
+    }
   })
+  
+  // gridElement.innerHTML = '<div class="grid-bg"></div>'
+  // store.state.grid.cells.forEach(({value}) => {
+  //   gridElement.innerHTML += `<div class="box ${value === 'set' ? 'active' : ''}"></div>`
+  // })
 }
--- src/store.js
+++ src/store.js
@@ -9,8 +9,8 @@ const state = {
 export const store = {
   state,
   doSomething () {
-    state.grid.setCellValueAtIndex(0, Cell.SET)
-    state.grid.setCellValueAtPoint(9, 3, Cell.SET)
+    // state.grid.setCellValueAtIndex(0, Cell.SET)
+    // state.grid.setCellValueAtPoint(9, 3, Cell.SET)
     state.grid.setCellValueAtPoint(Math.floor(Math.random()*10), Math.floor(Math.random()*10), Cell[Math.random() > 0.5 ? 'SET' : 'UNSET'])
   }
 }
